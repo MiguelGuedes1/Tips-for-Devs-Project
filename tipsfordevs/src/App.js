42ab9065d5653f894228d1745440de3161c952a9
MiguelGuedes1 <miguelsilvaguedes1@gmail.com>
2023-06-05 19:49:42 +0100

    Estabelecer Rotas

--- tipsfordevs/src/App.js
+++ tipsfordevs/src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
 
+import {BrowserRouter,Routes,Route,Navigate,Link} from "react-router-dom"
+
 function App() {
   return (
     <div className="App">
-     <h1>Tips for devs</h1>
+      <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route></Route>
+        </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
