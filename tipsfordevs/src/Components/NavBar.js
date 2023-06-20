5919b34c80b88c6bf068d781070683ffaabf3ff9
MiguelGuedes1 <miguelsilvaguedes1@gmail.com>
2023-06-20 20:15:18 +0100

    nav bar tel/tablet pronta

--- tipsfordevs/src/Components/NavBar.js
+++ tipsfordevs/src/Components/NavBar.js
@@ -1,31 +1,32 @@
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { UserAuthentication } from "../hooks/useAuthentication";
 import { useAuthValue } from "../context/AuthContext";
 import styles from "./NavBar.module.css";
-import { FaGithub, FaLinkedin, FaDesktop } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaDesktop, FaAlignJustify } from 'react-icons/fa';
 
 const NavBar = () => {
   const { user } = useAuthValue();
   const { logout } = UserAuthentication();
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const clicarLogo = () => {
     navigate('/');
   }
 
+  const abrirFecharMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
 
   return (
     <nav className={styles.navbar}>
       <span onClick={clicarLogo} className={styles.logo}>
-      <span className={styles.ctrl_logo}>CTRL</span> + Code <FaDesktop />
+        <span className={styles.ctrl_logo}>CTRL</span> + Code <FaDesktop />
       </span>
 
-     
-
       <ul className={styles.links_list}>
-  
-
-        {!user && (
+        {!user ? (
           // Acesso para utilizadores NÃO autenticados
           <>
             <li>
@@ -35,14 +36,12 @@ const NavBar = () => {
               <NavLink to="./Register">Register</NavLink>
             </li>
           </>
-        )}
-
-        {user && (
+        ) : (
           // Acesso para utilizadores AUTENTICADOS
           <>
             <li>
               <NavLink to="./">Home</NavLink> {/* Acesso Global */}
-           </li>
+            </li>
             <li>
               <NavLink to="./CreatePost">Create Post</NavLink>
             </li>
@@ -62,6 +61,37 @@ const NavBar = () => {
           </li>
         )}
       </ul>
+
+      
+      
+      
+      
+      
+      {/* Nav bar para telemovel e tablet */}
+      <div className={styles.navbar_telemovel_tablet}>
+  <span onClick={abrirFecharMenu}><FaAlignJustify /></span>
+
+  {menuOpen && (
+    <div>
+      {!user ? (
+        <>
+          <p><NavLink to="./Login">Login</NavLink></p>
+          <p><NavLink to="./Register">Register</NavLink></p>
+          <p><NavLink to="./About">About the Project</NavLink></p>
+        </>
+      ) : (
+        <>
+          <p><NavLink to="./">Home</NavLink></p>
+          <p><NavLink to="./CreatePost">Create Post</NavLink></p>
+          <p><NavLink to="./Dashboard">Your posts</NavLink></p>
+          <p><NavLink to="./About">About the Project</NavLink></p>
+          <p onClick={logout}>LogOut</p>
+        </>
+      )}
+    </div>
+  )}
+</div>
+
     </nav>
   );
 }
